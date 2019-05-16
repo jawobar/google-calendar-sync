68777f9093c1569fc1fde24c4589ea1872a8ded6
Bartosz Jaworski <bartosz.jaworski@gmail.com>
2019-05-16 22:58:48 +0200

    Include VueCal

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <calendar></calendar>
+  </div>
+</template>
+
+<script>
+import TheCalendar from "./components/TheCalendar";
+
+export default {
+  name: 'app',
+  components: {
+    calendar: TheCalendar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TheCalendar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <vue-cal locale="pl" class="vuecal--green-theme" style="height: 450px"></vue-cal>
+  </div>
+</template>
+
+<script>
+import VueCal from "vue-cal";
+import "vue-cal/dist/vuecal.css";
+
+export default {
+  name: "TheCalendar",
+  components: { VueCal }
+};
+</script>
+
+<style scoped></style>
