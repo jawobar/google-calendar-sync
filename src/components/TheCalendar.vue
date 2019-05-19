eaf6aaff9095fa7d2dc7443979fb18a64e5e5e81
Bartosz Jaworski <bartosz.jaworski@gmail.com>
2019-05-19 23:37:05 +0200

    Add first navbar

--- src/components/TheCalendar.vue
+++ src/components/TheCalendar.vue
@@ -1,6 +1,35 @@
 <template>
   <div class="container">
-    <vue-cal locale="pl" class="vuecal--green-theme" style="height: 450px"></vue-cal>
+    <b-navbar type="light" variant="light" fixed="top">
+      <b-navbar-brand href="#">Calendar Sync</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Authorize</b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item @click="language = 'pl'">PL</b-dropdown-item>
+            <b-dropdown-item @click="language = 'en'">EN</b-dropdown-item>
+            <b-dropdown-item @click="language = 'de'">DE</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template slot="button-content"><em>User</em></template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <vue-cal
+      :locale="language"
+      class="vuecal--green-theme"
+      style="height: 450px"
+    ></vue-cal>
   </div>
 </template>
 
@@ -10,8 +39,17 @@ import "vue-cal/dist/vuecal.css";
 
 export default {
   name: "TheCalendar",
+  data() {
+    return {
+      language: "pl"
+    }
+  },
   components: { VueCal }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.navbar {
+  /*margin-bottom: 10px;*/
+}
+</style>
